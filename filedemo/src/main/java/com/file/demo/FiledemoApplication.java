9d3ab3ebd1929e3e91ddb4ca8ed6c05d83cf1e33
jrpbjr <jrpbjr@gmail.com>
2020-10-26 13:35:36 -0300

    carregando as propriedades do sistema de arquivo

--- filedemo/src/main/java/com/file/demo/FiledemoApplication.java
+++ filedemo/src/main/java/com/file/demo/FiledemoApplication.java
@@ -2,8 +2,12 @@ package com.file.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.file.demo.storage.StorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class FiledemoApplication {
 
 	public static void main(String[] args) {
